ca41f80ddaad4204a1a7404c3ff7ea153533c0b2
Sachin Acharya <acharyaraj71+github_project@gmail.com>
2024-02-08 15:22:46 +0545

    Created API for scraping YouTube data using Python <--> Flask

--- Server/README.md
+++ Server/README.md
@@ -4,36 +4,206 @@ It is an API for Music Player that scrapes Data from YouTube and return
 
 ## Endpoints
 
-### Get video informations without playback url
+1. **Get audio informations without playback URL**
 
-1. **Endpoint:** `/api/video/<video-id>/no-url/` _[GET]_
+    - **ENDPOINT:**
+        ```bash
+        /api/video/no-url/
+        ```
+    - **METHODS:** `['POST']`
+    - **REQUEST:**
 
-### Get video informations with playback url
+        ```json
+        {
+            "src": "YouTube ID or YouTube URL of the video",
+            "id": "true if src is ID otherwise false. Default is false"
+        }
+        ```
 
-2. **Endpoint:** `/api/video/<video-id/` _[GET]_
+    - **RESPONSE:**
 
----
+        ```json
+        {
+            "title": "Title of the Audio (YouTube Title)",
+            "thumbnail": "URL of the YouTube thumbnail for the video",
+            "channel": "YouTube channel name of this video",
+            "duration": "Duration of the video: (hr?:min:sec)",
+            "video_id": "YouTube ID of the video",
+            "status": "success"
+        }
+        ```
 
-### Get playlist videos informations without playback url
+        or
 
-3. **Endpoint:** `/api/playlist/<playlist-id>/no-url/` _[GET]_
+        ```json
+        {
+            "status": "Invalid Request",
+            "message": "Message about the issue"
+        }
+        ```
 
-### Get playlist videos informations with playback url
+2. **Get audio informations with playback URL**
 
-4. **Endpoint:** `/api/playlist/<playlist-id>/` _[GET]_
+    - **ENDPOINT:**
+        ```bash
+        /api/video/
+        ```
+    - **METHODS:** `['POST']`
+    - **REQUEST:**
 
----
+        ```json
+        {
+            "src": "YouTube ID or YouTube URL of the video",
+            "id": "true if src is ID otherwise false. Default is false"
+        }
+        ```
 
-### Search YouTube video
+    - **RESPONSE:**
 
-5. **Endpoint:** `/api/search/video/<video-title>/` _[POST]_
+        ```json
+        {
+            "title": "Title of the Audio (YouTube Title)",
+            "thumbnail": "URL of the YouTube thumbnail for the video",
+            "channel": "YouTube channel name of this video",
+            "duration": "Duration of the video: (hr?:min:sec)",
+            "video_id": "YouTube ID of the video",
+            "status": "success",
+            "stream": {
+                "url": "Stream URL of the audio file",
+                "expiration": "Video Expire time - proper formatted, Example: Thu, 08 Feb 2024 14:14:42 GMT"
+            }
+        }
+        ```
 
----
+        or
 
-### Get playback URL
+        ```json
+        {
+            "status": "Invalid Request",
+            "message": "Message about the issue"
+        }
+        ```
 
-6. **Endpoint: ** `/api/video/playback/<video-id>/` **[GET]**
+3. **Get audio stream url**
 
+    - **ENDPOINT:**
+        ```bash
+        /api/video/stream/
+        ```
+    - **METHODS:** `['POST']`
+    - **REQUEST:**
 
+        ```json
+        {
+            "src": "YouTube ID or YouTube URL of the video",
+            "id": "true if src is ID otherwise false. Default is false"
+        }
+        ```
 
-> (Note: For faster response, always use endpoint that returns without playback url as extracting playback url is slow process and thus decrease the API response time)\_
+    - **RESPONSE:**
+
+        ```json
+        {
+            "status": "success",
+            "stream": {
+                "url": "Stream URL of the audio file",
+                "expiration": "Video Expire time - proper formatted, Example: Thu, 08 Feb 2024 14:14:42 GMT"
+            }
+        }
+        ```
+
+        or
+
+        ```json
+        {
+            "status": "Invalid Request",
+            "message": "Message about the issue"
+        }
+        ```
+
+4. **Get playlist videos informations without playback URL**
+
+    - **ENDPOINT:**
+        ```bash
+        /api/playlist/no-url/
+        ```
+    - **METHODS:** `['POST']`
+    - **REQUEST:**
+
+        ```json
+        {
+            "src": "YouTube ID or YouTube URL of the video",
+            "id": "true if src is ID otherwise false. Default is false"
+        }
+        ```
+
+    - **RESPONSE:**
+
+        ```json
+        {
+            "status": "success",
+            "videos": [
+                {
+                    "title": "Title of the Audio (YouTube Title)",
+                    "thumbnail": "URL of the YouTube thumbnail for the video",
+                    "channel": "YouTube channel name of this video",
+                    "duration": "Duration of the video: (hr?:min:sec)",
+                    "video_id": "YouTube ID of the video"
+                },
+                ...
+            ]
+        }
+        ```
+
+        or
+
+        ```json
+        {
+            "status": "Invalid Request",
+            "message": "Message about the issue"
+        }
+        ```
+
+5. **Get playlist videos informations without playback URL**
+   This method is not available as it is too tasking to get stream url for each videos especially for the playlist with large number of videos.
+
+6. **Get list of YouTube Searches**
+
+    - **ENDPOINT:**
+        ```bash
+        /api/search/video/
+        ```
+    - **METHODS:** `['POST']`
+    - **REQUEST:**
+
+        ```json
+        {
+            "src": "Search string to search on YouTube",
+            "limit": "Number of result required. Default is 5"
+        }
+        ```
+
+    - **RESPONSE:**
+
+        ```json
+        {
+            "status": "success",
+            "videos": {
+                "title": "Title of the Audio (YouTube Title)",
+                "thumbnail": "URL of the YouTube thumbnail for the video",
+
+                "channel": "YouTube channel name of this video",
+                "duration": "Duration of the video: (hr?:min:sec)",
+                "video_id": "YouTube ID of the video"
+            }
+        }
+        ```
+
+        or
+
+        ```json
+        {
+            "status": "Invalid Request",
+            "message": "Message about the issue"
+        }
+        ```
--- Server/main.py
+++ Server/main.py
@@ -1,55 +1,229 @@
-from flask import Flask, jsonify
-
-app = Flask(
-    __name__
+from flask import Flask, jsonify, request
+from flask_caching import Cache
+from youtubesearchpython import (
+    Video,
+    Playlist,
+    VideosSearch,
 )
+from pytube import YouTube
+
+app = Flask(__name__)
+app.config["CACHE_TYPE"] = "simple"
+cache = Cache(app)
+
+video_url = "https://www.youtube.com/watch?v=%s"
+playlist_url = "https://www.youtube.com/playlist?list=%s"
+
+
+def maxThumbnail(thumbnails: list[dict]):
+    return max(thumbnails, key=lambda x: x["width"])["url"]
+
+
+def convert_seconds(seconds: int | float):
+    hours = seconds // (3600)
+    remainder = seconds % 3600
+    minutes = remainder // 60
+    remainder = remainder % 60
+
+    if hours > 0:
+        # return f"{hours:02d}:{minutes:02d}:{remainder:02d}"
+        return f"{hours}:{minutes}:{remainder}"
+    else:
+        # return f"{minutes:02d}:{remainder:02d}"
+        return f"{minutes}:{remainder}"
+
+
+def parseVideoInformation(obj: dict, isPlaylist: bool = False):
+    return {
+        "title": obj["title"],
+        "thumbnail": maxThumbnail(obj["thumbnails"]),
+        "channel": obj["channel"]["name"],
+        "duration": (
+            obj["duration"]
+            if isPlaylist
+            else convert_seconds(int(obj["duration"]["secondsText"]))
+        ),
+        "video_id": obj["id"],
+    }
+
 
 # video information without playback url
-@app.route("/api/video/<video_id>/no-url/")
-def video_no_playback(video_id: str):
-    return jsonify({
-        "video_id": video_id,
-        "no-url": True
-    })
-# video information with playback url
-@app.route("/api/video/<video_id>/")
-def video(video_id: str):
-    return jsonify({
-        "video_id": video_id,
-        "no-url": False
-    })
+@app.route("/api/video/no-url/", methods=["POST"])
+def video_no_playback():
+    if request.method == "POST":
+        src = request.form.get("src", None)
+        isID = bool(request.form.get("id", False))
+        if not src:
+            return (
+                jsonify(
+                    {
+                        "status": "Invalid Request",
+                        "message": "Argument 'src' is required. It can be Video URL or Video ID",
+                    }
+                ),
+                400,
+            )
+        if isID:
+            src = video_url % src
+        video_info = Video.getInfo(src)
+        return jsonify({"status": "success", **parseVideoInformation(video_info)}), 200
+    return (
+        jsonify(
+            {"status": "Invalid Request", "message": "Only POST request is allowed"}
+        ),
+        405,
+    )
 
-# video playback url
-@app.route("/api/video/playback/<video_id>")
-def video_playback(video_id):
-    return jsonify({
-        "video_id": video_id,
-        "no-url": False
-    })
+
+# video information with playback url
+@app.route("/api/video/", methods=["POST"])
+@cache.cached(timeout=300)  # For 5 minutes
+def video():
+    if request.method == "POST":
+        src = request.form.get("src", None)
+        isID = bool(request.form.get("id", False))
+        if not src:
+            return (
+                jsonify(
+                    {
+                        "status": "Invalid Request",
+                        "message": "Argument 'src' is required. It can be Video URL or Video ID",
+                    }
+                ),
+                400,
+            )
+        if isID:
+            src = video_url % src
+        video_src = YouTube(src).streams.get_audio_only()
+        video_info = Video.getInfo(src)
+        return (
+            jsonify(
+                {
+                    "status": "success",
+                    **parseVideoInformation(video_info),
+                    "stream": {
+                        "url": video_src.url,
+                        "expiration": video_src.expiration,
+                    },
+                }
+            ),
+            200,
+        )
+    return (
+        jsonify(
+            {"status": "Invalid Request", "message": "Only POST request is allowed"}
+        ),
+        405,
+    )
+
+
+# video stream url
+@app.route("/api/video/stream/", methods=["POST"])
+@cache.cached(timeout=300)
+def video_playback():
+    if request.method == "POST":
+        src = request.form.get("src", None)
+        isID = bool(request.form.get("id", False))
+
+        if not src:
+            return (
+                jsonify(
+                    {
+                        "status": "Invalid Request",
+                        "message": "Argument 'src' is required. It can be Video URL or Video ID",
+                    }
+                ),
+                400,
+            )
+        if isID:
+            src = video_url % src
+        video_src = YouTube(src).streams.get_audio_only()
+        return jsonify(
+            {
+                "status": "success",
+                "stream": {"url": video_src.url, "expiration": video_src.expiration},
+            }
+        )
+    return (
+        jsonify(
+            {"status": "Invalid Request", "message": "Only POST request is allowed"}
+        ),
+        405,
+    )
+
 
 # Playlist videos without playback url
-@app.route("/api/playlist/<playlist_id>/no-url/")
-def playlist_no_playback(playlist_id: str):
-    return jsonify({
-        "playlist_id": playlist_id,
-        "no-url": True
-    })
-# Playlist videos with playback url
-@app.route("/api/playlist/<playlist_id>/")
-def playlist(playlist_id: str):
-    return jsonify({
-        "playlist_id": playlist_id,
-        "no-url": False
-    })
+@app.route("/api/playlist/no-url/", methods=["POST"])
+def playlist_no_playback():
+    if request.method == "POST":
+        src = request.form.get("src", None)
+        isID = bool(request.form.get("id", False))
+        if not src:
+            return (
+                jsonify(
+                    {
+                        "status": "Invalid Request",
+                        "message": "Argument 'src' is required. It can be Video URL or Video ID",
+                    }
+                ),
+                400,
+            )
+        if isID:
+            src = playlist_url % src
+        playlist_info = Playlist(src)
+        while playlist_info.hasMoreVideos:
+            playlist_info.getNextVideos()
+        video_info = list(
+            map(lambda x: parseVideoInformation(x, True), playlist_info.videos)
+        )
+        return jsonify({"status": "success", "videos": video_info}), 200
+    return (
+        jsonify(
+            {"status": "Invalid Request", "message": "Only POST request is allowed"}
+        ),
+        405,
+    )
+
 
 # Search Videos (From Title)
-@app.route("/api/search/video/<video_title>/")
-def search_video(video_title: str):
-    return jsonify({
-        "video_title": video_title,
-        "no-url": False
-    })
-    
+@app.route("/api/search/video/", methods=["POST"])
+def search_video():
+    if request.method == "POST":
+        src = request.form.get("src", None)
+        limit = request.form.get("limit", "5")
+        if not src:
+            return (
+                jsonify(
+                    {
+                        "status": "Invalid Request",
+                        "message": "Argument 'src' is required. It can be Video URL or Video ID",
+                    }
+                ),
+                400,
+            )
+        if not limit.isdigit():
+            return (
+                jsonify(
+                    {
+                        "status": "Invalid Request",
+                        "message": "Argument 'limit' must be digit",
+                    }
+                ),
+                400,
+            )
+        video_info = VideosSearch(src, limit=int(limit)).result()
+        print(video_info)
+        video_info = list(
+            map(lambda x: parseVideoInformation(x, True), video_info["result"])
+        )
+        return jsonify({"status": "success", "videos": video_info}), 200
+    return (
+        jsonify(
+            {"status": "Invalid Request", "message": "Only POST request is allowed"}
+        ),
+        405,
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
